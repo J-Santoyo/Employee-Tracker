b4e622cdfd88ffdc189a244ee86d0bdaf647a5fa
J-Santoyo <joanasantoyo030@gmail.com>
2022-03-09 15:42:13 -0700

    created company db

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
